38ff9d25371528e093c76983cbaf9fecadc35f04
Ioannis Konstantelias <ioannis.konstantelias@camlingroup.com>
2024-11-08 17:52:53 +0000

    Add initial clang-format rules

---
+++ scripts/check_format.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+commit=$1
+
+cmd="git diff -U0 --no-color $commit | clang-format-diff -p1"
+diff=$(eval "$cmd")
+if [ $? -ne 0 ]
+then
+  echo "!!! Failed to check the patch for formatting issues"
+  exit 1
+fi
+
+if [ -z "$diff" ]
+then
+  echo "Well done"
+  exit 0
+else
+  echo "!!! Formatting inconsistencies"
+  echo "!!! Please, format the code"
+  echo
+  echo "$diff"
+  exit 1
+fi
